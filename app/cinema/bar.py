1ed5b6e5b5da2feeb9df2bd9de12006b3278298f
Maksym Zhmura <zmuramaksim@gmail.com>
2026-01-31 21:36:27 +0200

    Solution5

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 
 class CinemaBar:
     @staticmethod
-    def sell_product(customer: str, product: str) -> None:
+    def sell_product(product: str, customer: str) -> None:
         print(f"Cinema bar sold {product} to {customer}.")
